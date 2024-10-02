0273a7e2ce8e144643b766c5494c87eb14df4cb0
Andres Rodriguez <banrostin@gmail.com>
2024-10-02 14:08:01 -0600

    Explore movies render

--- front/scripts/explorePelis.js
+++ front/scripts/explorePelis.js
@@ -1,11 +1,12 @@
 import renderMovies from './renderMovies.js';
 const d = document;
 
-
+ const backURL = window.location.hostname === "localhost" || window.location.hostname === '127.0.0.1' ?
+    'http://localhost:3000/movies' : 'https://app-cinema-back.onrender.com/movies'
 
 export default async function explorePelis (){
     try {
-     const response = await axios.get("http://localhost:3000/movies");
+     const response = await axios.get(`${backURL}`);
      const dataMovies = response.data;
      renderMovies(dataMovies);
     } catch (error) {
