1be1bc868b004dd2ffbc9a7a210bc0de92bc0b0a
Andres Rodriguez <banrostin@gmail.com>
2024-10-02 14:04:20 -0600

    Explore movies correcion

--- front/scripts/exploreMovies.js
+++ front/scripts/exploreMovies.js
@@ -2,10 +2,12 @@ import renderMovies from './renderMovies.js';
 const d = document;
 const inicioinfo = d.getElementById('inicioInfo');
 
+ const backURL = window.location.hostname === "localhost" || window.location.hostname === '127.0.0.1' ?
+    'http://localhost:3000/movies' : 'https://app-cinema-back.onrender.com/movies'
 
 export default async function exploreMovies (){
     try {
-     const response = await axios.get("http://localhost:3000/movies");
+     const response = await axios.get(`${backURL}`);
      const dataMovies = response.data;
      inicioinfo.innerHTML = '';
      renderMovies(dataMovies);
